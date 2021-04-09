4ad040002b24eb4e0c5b4e467ded365fab6e3b7e
EdwardLim78 <weichun78@hotmail.com>
2021-04-09 19:48:39 +0800

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DafaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
 
         }
 
@@ -47,6 +48,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
